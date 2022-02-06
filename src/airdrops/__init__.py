9df8ea6741a49d389b1628cc8bce99161d5c8554
amiwrpremium <amiwrpremium@gmail.com>
2022-02-06 15:03:32 +0330

    add to main

--- src/airdrops/__init__.py
+++ src/airdrops/__init__.py
@@ -1,3 +1,4 @@
 from . import create_wallet as create_wallet_entrance
 from . import set_trustline as set_trustline_entrance
 from . import create_order as create_order_entrance
+from . import cancel_order as cancel_order_entrance
